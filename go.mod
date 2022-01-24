06f940a79f8b961d3dfca28272cd6d0949abbe17
Michael Lechner <michael.lechner@twint.ch>
2022-01-24 10:55:25 +0100

    change to pulumi eks module, not working yet

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module aws-go-eks
 go 1.13
 
 require (
-	github.com/pulumi/pulumi-aws/sdk/v4 v4.0.0
+	github.com/pulumi/pulumi-aws/sdk v1.31.0
+	github.com/pulumi/pulumi-aws/sdk/v4 v4.35.0
+	github.com/pulumi/pulumi-eks/sdk v0.36.0
 	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.0.0
-	github.com/pulumi/pulumi/sdk/v3 v3.0.0
+	github.com/pulumi/pulumi/sdk/v3 v3.14.0
 )
--- main.go
+++ main.go
@@ -1,21 +1,97 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
-	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
-	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/eks"
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/iam"
-	// appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	"github.com/pulumi/pulumi-eks/sdk/go/eks"
+	k8s "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v2"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
-	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/providers"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/yaml"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		// Per NodeGroup IAM: each NodeGroup will bring its own, specific instance role and profile.
+		managedPolicyArns := []string{
+			"arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+			"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+			"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+		}
+
+		// Creates a role and attaches the EKS worker node IAM managed policies. Used a few times below,
+		// to create multiple roles, so we use a function to avoid repeating ourselves.
+		createRole := func(name string) (*iam.Role, error) {
+			instance_assume_role_policy, err := iam.GetPolicyDocument(ctx, &iam.GetPolicyDocumentArgs{
+				Statements: []iam.GetPolicyDocumentStatement{
+					{
+						Actions: []string{
+							"sts:AssumeRole",
+						},
+						Principals: []iam.GetPolicyDocumentStatementPrincipal{
+							{
+								Type: "Service",
+								Identifiers: []string{
+									"ec2.amazonaws.com",
+								},
+							},
+						},
+					},
+				},
+			}, nil)
+			if err != nil {
+				return nil, err
+			}
+
+			role, err := iam.NewRole(ctx, name, &iam.RoleArgs{
+				AssumeRolePolicy: pulumi.String(instance_assume_role_policy.Json),
+				Name:             pulumi.String(name),
+			})
+			if err != nil {
+				return nil, err
+			}
+
+			counter := 0
+			for _, policy := range managedPolicyArns {
+				// Create RolePolicyAttachment without returning it.
+				_, err := iam.NewRolePolicyAttachment(ctx,
+					fmt.Sprintf("%s-policy-%d", name, counter),
+					&iam.RolePolicyAttachmentArgs{
+						PolicyArn: pulumi.String(policy),
+						Role:      role.Name,
+					},
+				)
+				if err != nil {
+					return nil, err
+				}
+				counter++
+			}
+
+			return role, nil
+		}
+
+		jsonFile, err := ioutil.ReadFile("elb-policy.json")
+		if err != nil {
+			return err
+		}
+
+		json0 := string(jsonFile)
+		_, err = iam.NewPolicy(ctx, "AWSLoadBalancerControllerIAMPolicy", &iam.PolicyArgs{
+			Path:        pulumi.String("/"),
+			Description: pulumi.String("AWSLoadBalancerControllerIAMPolicy"),
+			Policy:      pulumi.String(json0),
+		})
+		if err != nil {
+			return err
+		}
+
 		// Read back the default VPC and public subnets, which we will use.
 		t := true
 		vpc, err := ec2.LookupVpc(ctx, &ec2.LookupVpcArgs{Default: &t})
@@ -26,89 +102,6 @@ func main() {
 		if err != nil {
 			return err
 		}
-		eksRole, err := iam.NewRole(ctx, "eks-iam-eksRole", &iam.RoleArgs{
-			AssumeRolePolicy: pulumi.String(`{
-		    "Version": "2008-10-17",
-		    "Statement": [{
-		        "Sid": "",
-		        "Effect": "Allow",
-		        "Principal": {
-		            "Service": "eks.amazonaws.com"
-		        },
-		        "Action": "sts:AssumeRole"
-		    }]
-		}`),
-		})
-		if err != nil {
-			return err
-		}
-		eksPolicies := []string{
-			"arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
-			"arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-		}
-		for i, eksPolicy := range eksPolicies {
-			_, err := iam.NewRolePolicyAttachment(ctx, fmt.Sprintf("rpa-%d", i), &iam.RolePolicyAttachmentArgs{
-				PolicyArn: pulumi.String(eksPolicy),
-				Role:      eksRole.Name,
-			})
-			if err != nil {
-				return err
-			}
-		}
-		// Create the EC2 NodeGroup Role
-		nodeGroupRole, err := iam.NewRole(ctx, "nodegroup-iam-role", &iam.RoleArgs{
-			AssumeRolePolicy: pulumi.String(`{
-		    "Version": "2012-10-17",
-		    "Statement": [{
-		        "Sid": "",
-		        "Effect": "Allow",
-		        "Principal": {
-		            "Service": "ec2.amazonaws.com"
-		        },
-		        "Action": "sts:AssumeRole"
-		    }]
-		}`),
-		})
-		if err != nil {
-			return err
-		}
-		nodeGroupPolicies := []string{
-			"arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-			"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-			"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-		}
-		for i, nodeGroupPolicy := range nodeGroupPolicies {
-			_, err := iam.NewRolePolicyAttachment(ctx, fmt.Sprintf("ngpa-%d", i), &iam.RolePolicyAttachmentArgs{
-				Role:      nodeGroupRole.Name,
-				PolicyArn: pulumi.String(nodeGroupPolicy),
-			})
-			if err != nil {
-				return err
-			}
-		}
-		// Create a Security Group that we can use to actually connect to our cluster
-		clusterSg, err := ec2.NewSecurityGroup(ctx, "test-cluster-sg", &ec2.SecurityGroupArgs{
-			VpcId: pulumi.String(vpc.Id),
-			Egress: ec2.SecurityGroupEgressArray{
-				ec2.SecurityGroupEgressArgs{
-					Protocol:   pulumi.String("-1"),
-					FromPort:   pulumi.Int(0),
-					ToPort:     pulumi.Int(0),
-					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-				},
-			},
-			Ingress: ec2.SecurityGroupIngressArray{
-				ec2.SecurityGroupIngressArgs{
-					Protocol:   pulumi.String("tcp"),
-					FromPort:   pulumi.Int(80),
-					ToPort:     pulumi.Int(80),
-					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
 
 		eksClusters := []string{
 			"test",
@@ -116,55 +109,56 @@ func main() {
 		}
 
 		for _, env := range eksClusters {
-		// Create EKS Cluster
-			eksCluster, err := eks.NewCluster(ctx, fmt.Sprintf("%s-aws-demo", env), &eks.ClusterArgs{
-				RoleArn: pulumi.StringInput(eksRole.Arn),
-				VpcConfig: &eks.ClusterVpcConfigArgs{
-					PublicAccessCidrs: pulumi.StringArray{
-						pulumi.String("0.0.0.0/0"),
-					},
-					SecurityGroupIds: pulumi.StringArray{
-						clusterSg.ID().ToStringOutput(),
-					},
-					SubnetIds: toPulumiStringArray(subnet.Ids),
-				},
+
+			role, err := createRole(fmt.Sprintf("%s-node-role", env))
+			if err != nil {
+				return err
+			}
+			_, err = iam.NewInstanceProfile(ctx, fmt.Sprintf("%s-instance-profile", env),
+				&iam.InstanceProfileArgs{Role: role})
+			if err != nil {
+				return err
+			}
+
+			// Create an EKS cluster with the many IAM roles to register with the cluster auth.
+			cluster, err := eks.NewCluster(ctx, fmt.Sprintf("%s-aws-demo", env), &eks.ClusterArgs{
+				SkipDefaultNodeGroup: pulumi.Bool(true),
+				CreateOidcProvider:   pulumi.Bool(true),
 			})
 			if err != nil {
 				return err
 			}
 
-			// iamOidcProvider, err := eks.NewIdentityProviderConfig(ctx, "iam-oidc-provicer", &eks.IdentityProviderConfigArgs{
-			// 	ClusterName: eksCluster.Name,	
-			// })
-			// if err != nil {
-			// 	return err
-			// }
-			// if iamOidcProvider == nil {
-			// 	return err
-			// }
-
-			nodeGroup, err := eks.NewNodeGroup(ctx, fmt.Sprintf("%s-aws-demo-node-group", env), &eks.NodeGroupArgs{
-				ClusterName:   eksCluster.Name,
-				NodeGroupName: pulumi.String(fmt.Sprintf("%s-aws-demo-node-group", env)),
-				NodeRoleArn:   pulumi.StringInput(nodeGroupRole.Arn),
-				SubnetIds:     toPulumiStringArray(subnet.Ids),
-				ScalingConfig: &eks.NodeGroupScalingConfigArgs{
-					DesiredSize: pulumi.Int(3),
-					MaxSize:     pulumi.Int(6),
-					MinSize:     pulumi.Int(1),
-				},
+			// Create a Kubernetes provider using the new cluster's Kubeconfig.
+			eksProvider, err := k8s.NewProvider(ctx, fmt.Sprintf("%s-eksProvider", env), &k8s.ProviderArgs{
+				Kubeconfig: cluster.Kubeconfig.ApplyT(
+					func(config interface{}) (string, error) {
+						b, err := json.Marshal(config)
+						if err != nil {
+							return "", err
+						}
+						return string(b), nil
+					}).(pulumi.StringOutput),
 			})
 			if err != nil {
 				return err
 			}
+			eksProviders := pulumi.ProviderMap(map[string]pulumi.ProviderResource{
+				"kubernetes": eksProvider,
+			})
 
-			ctx.Export(fmt.Sprintf("%sKubeconfig", env), generateKubeconfig(eksCluster.Endpoint,
-				eksCluster.CertificateAuthority.Data().Elem(), eksCluster.Name))
-
-			k8sProvider, err := providers.NewProvider(ctx, fmt.Sprintf("%s-k8sprovider", env), &providers.ProviderArgs{
-				Kubeconfig: generateKubeconfig(eksCluster.Endpoint,
-					eksCluster.CertificateAuthority.Data().Elem(), eksCluster.Name),
-			}, pulumi.DependsOn([]pulumi.Resource{nodeGroup}))
+			// First, create a node group for fixed compute.
+			_, err = eks.NewNodeGroup(ctx, fmt.Sprintf("%s-aws-demo-ng1", env), &eks.NodeGroupArgs{
+				Cluster:         cluster.Core,
+				InstanceType:    pulumi.String("t2.small"),
+				DesiredCapacity: pulumi.Int(3),
+				MinSize:         pulumi.Int(1),
+				MaxSize:         pulumi.Int(3),
+				// Labels: pulumi.StringMap{
+				// 	"ondemand": pulumi.String("true"),
+				// },
+				// InstanceProfile: instanceProfile,
+			}, eksProviders)
 			if err != nil {
 				return err
 			}
@@ -173,14 +167,14 @@ func main() {
 				Metadata: &metav1.ObjectMetaArgs{
 					Name: pulumi.String("argocd"),
 				},
-			}, pulumi.Provider(k8sProvider))
+			}, pulumi.Provider(eksProvider))
 			if err != nil {
 				return err
 			}
 
-			_, err1 := helm.NewChart(ctx, fmt.Sprintf("%s-argo-cd", env), helm.ChartArgs{
-				Chart:     pulumi.String("argo-cd"),
-				Namespace: pulumi.String("argocd"),
+			_, err = helm.NewChart(ctx, fmt.Sprintf("%s-argo-cd", env), helm.ChartArgs{
+				Chart:          pulumi.String("argo-cd"),
+				Namespace:      pulumi.String("argocd"),
 				ResourcePrefix: env,
 				FetchArgs: helm.FetchArgs{
 					Repo: pulumi.String("https://argoproj.github.io/argo-helm"),
@@ -192,14 +186,14 @@ func main() {
 						},
 					},
 				},
-			}, pulumi.Provider(k8sProvider), pulumi.DependsOn([]pulumi.Resource{argocdNamespace}))
-			if err1 != nil {
+			}, pulumi.Provider(eksProvider), pulumi.DependsOn([]pulumi.Resource{argocdNamespace}))
+			if err != nil {
 				return err
 			}
 
-			_, err3 := helm.NewChart(ctx, fmt.Sprintf("%s-argo-rollouts", env), helm.ChartArgs{
-				Chart:     pulumi.String("argo-rollouts"),
-				Namespace: pulumi.String("argocd"),
+			_, err = helm.NewChart(ctx, fmt.Sprintf("%s-argo-rollouts", env), helm.ChartArgs{
+				Chart:          pulumi.String("argo-rollouts"),
+				Namespace:      pulumi.String("argocd"),
 				ResourcePrefix: env,
 				FetchArgs: helm.FetchArgs{
 					Repo: pulumi.String("https://argoproj.github.io/argo-helm"),
@@ -207,126 +201,69 @@ func main() {
 				Values: pulumi.Map{
 					"dashboard": pulumi.Map{
 						"enabled": pulumi.String("true"),
-						},
+					},
 				},
-			}, pulumi.Provider(k8sProvider), pulumi.DependsOn([]pulumi.Resource{argocdNamespace}))
-			if err3 != nil {
+			}, pulumi.Provider(eksProvider), pulumi.DependsOn([]pulumi.Resource{argocdNamespace}))
+			if err != nil {
 				return err
 			}
 
-			_, err2 := corev1.NewNamespace(ctx, fmt.Sprintf("%s-app-ns", env), &corev1.NamespaceArgs{
+			_, err = corev1.NewNamespace(ctx, fmt.Sprintf("%s-app-ns", env), &corev1.NamespaceArgs{
 				Metadata: &metav1.ObjectMetaArgs{
 					Name: pulumi.String(fmt.Sprintf("%s-app", env)),
 				},
-			}, pulumi.Provider(k8sProvider))
-			if err2 != nil {
+			}, pulumi.Provider(eksProvider))
+			if err != nil {
 				return err
 			}
+
+			// Export the cluster's kubeconfig.
+			ctx.Export(fmt.Sprintf("%s-kubeconfig", env), cluster.Kubeconfig)
+
+			_, err = corev1.NewServiceAccount(ctx, fmt.Sprintf("%s-iam-serviceaccount", env), &corev1.ServiceAccountArgs{
+				Metadata: &metav1.ObjectMetaArgs{
+					Name:      pulumi.String("aws-load-balancer-controller"),
+					Namespace: pulumi.String("kube-system"),
+					Annotations: pulumi.StringMap{
+						"eks.amazonaws.com/role-arn": pulumi.String("arn:aws:iam::policy/AWSLoadBalancerControllerIAMPolicy"),
+					},
+				},
+			}, pulumi.Provider(eksProvider))
+			if err != nil {
+				return err
+			}
+
+			_, err = yaml.NewConfigFile(ctx, fmt.Sprintf("%s-elb-crd", env), &yaml.ConfigFileArgs{
+				File:           "aws-elb-crd.yaml",
+				ResourcePrefix: env,
+			}, pulumi.Provider(eksProvider))
+			if err != nil {
+				return err
+			}
+
+			_, err = helm.NewChart(ctx, fmt.Sprintf("%s-aws-elb", env), helm.ChartArgs{
+				Chart:          pulumi.String("aws-load-balancer-controller"),
+				Namespace:      pulumi.String("kube-system"),
+				ResourcePrefix: env,
+				FetchArgs: helm.FetchArgs{
+					Repo: pulumi.String("https://aws.github.io/eks-charts"),
+				},
+				Values: pulumi.Map{
+					"clusterName": cluster.Core,
+					"serviceAccount": pulumi.Map{
+						"create": pulumi.Bool(false),
+						"name":   pulumi.String("aws-load-balancer-controller"),
+					},
+					"image": pulumi.Map{
+						"tag": pulumi.String("v2.3.0"),
+					},
+				},
+			}, pulumi.Provider(eksProvider))
+			if err != nil {
+				return err
+			}
+
 		}
-
-		// appLabels := pulumi.StringMap{
-		// 	"app": pulumi.String("iac-workshop"),
-		// }
-		// _, err = appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
-		// 	Metadata: &metav1.ObjectMetaArgs{
-		// 		Namespace: namespace.Metadata.Elem().Name(),
-		// 	},
-		// 	Spec: appsv1.DeploymentSpecArgs{
-		// 		Selector: &metav1.LabelSelectorArgs{
-		// 			MatchLabels: appLabels,
-		// 		},
-		// 		Replicas: pulumi.Int(3),
-		// 		Template: &corev1.PodTemplateSpecArgs{
-		// 			Metadata: &metav1.ObjectMetaArgs{
-		// 				Labels: appLabels,
-		// 			},
-		// 			Spec: &corev1.PodSpecArgs{
-		// 				Containers: corev1.ContainerArray{
-		// 					corev1.ContainerArgs{
-		// 						Name:  pulumi.String("iac-workshop"),
-		// 						Image: pulumi.String("jocatalin/kubernetes-bootcamp:v2"),
-		// 					}},
-		// 			},
-		// 		},
-		// 	},
-		// }, pulumi.Provider(k8sProvider))
-		// if err != nil {
-		// 	return err
-		// }
-
-		// service, err := corev1.NewService(ctx, "app-service", &corev1.ServiceArgs{
-		// 	Metadata: &metav1.ObjectMetaArgs{
-		// 		Namespace: namespace.Metadata.Elem().Name(),
-		// 		Labels:    appLabels,
-		// 	},
-		// 	Spec: &corev1.ServiceSpecArgs{
-		// 		Ports: corev1.ServicePortArray{
-		// 			corev1.ServicePortArgs{
-		// 				Port:       pulumi.Int(80),
-		// 				TargetPort: pulumi.Int(8080),
-		// 			},
-		// 		},
-		// 		Selector: appLabels,
-		// 		Type:     pulumi.String("LoadBalancer"),
-		// 	},
-		// }, pulumi.Provider(k8sProvider))
-		// if err != nil {
-		// 	return err
-		// }
-
-		// ctx.Export("url", service.Status.ApplyT(func(status *corev1.ServiceStatus) *string {
-		// 	ingress := status.LoadBalancer.Ingress[0]
-		// 	if ingress.Hostname != nil {
-		// 		return ingress.Hostname
-		// 	}
-		// 	return ingress.Ip
-		// }))
-
 		return nil
 	})
-}
-
-//Create the KubeConfig Structure as per https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
-func generateKubeconfig(clusterEndpoint pulumi.StringOutput, certData pulumi.StringOutput, clusterName pulumi.StringOutput) pulumi.StringOutput {
-	return pulumi.Sprintf(`{
-        "apiVersion": "v1",
-        "clusters": [{
-            "cluster": {
-                "server": "%s",
-                "certificate-authority-data": "%s"
-            },
-            "name": "kubernetes",
-        }],
-        "contexts": [{
-            "context": {
-                "cluster": "kubernetes",
-                "user": "aws",
-            },
-            "name": "aws",
-        }],
-        "current-context": "aws",
-        "kind": "Config",
-        "users": [{
-            "name": "aws",
-            "user": {
-                "exec": {
-                    "apiVersion": "client.authentication.k8s.io/v1alpha1",
-                    "command": "aws-iam-authenticator",
-                    "args": [
-                        "token",
-                        "-i",
-                        "%s",
-                    ],
-                },
-            },
-        }],
-    }`, clusterEndpoint, certData, clusterName)
-}
-
-func toPulumiStringArray(a []string) pulumi.StringArrayInput {
-	var res []pulumi.StringInput
-	for _, s := range a {
-		res = append(res, pulumi.String(s))
-	}
-	return pulumi.StringArray(res)
 }
